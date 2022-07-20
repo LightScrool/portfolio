d424d5ede72b8cb1a0bd8457858d5fb955f3c8f3
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-07-20 17:46:01 +0500

    sections margin

--- client/src/components/About/index.tsx
+++ client/src/components/About/index.tsx
@@ -10,28 +10,30 @@ const About = () => {
     const text = "На данный момент я учусь в НИУ ВШЭ на «Программнай инженерии», год окончания - 2025 \n Учусь на бюджете, благодаря 288 баллам за ЕГЭ: \n Математика - 99 \n Информатика - 96 \n Русский язык- 93"
 
     return (
-        <Container>
-            <div className="About">
-                <ImageInsideVector
-                    className="About__photo"
-                    imageURL={photo}
-                    maskVector={<SVG Id={SVGIds.maskFrame2}/>}
-                    viewBox="0 0 100 82"
-                    imageTransform={"scale(1.45) translate(-12, -9.3)"}
-                />
+        <section className="About">
+            <Container>
+                <div className="About__inner">
+                    <ImageInsideVector
+                        className="About__photo"
+                        imageURL={photo}
+                        maskVector={<SVG Id={SVGIds.maskFrame2}/>}
+                        viewBox="0 0 100 82"
+                        imageTransform={"scale(1.45) translate(-12, -9.3)"}
+                    />
 
-                <div className="About__text-blocks">
-                    <AboutTextBlock
-                        title="Кратко обо мне"
-                        text="Программированием я начал интересоваться ещё в 8 классе и долгое время не мог определиться с направлением, поэтому на хорошем базовом уровне я владею несколькими языками программирования, в том числе: C#, Python и JS. В итоге я выбрал Front-end разработку в качестве направления своего развития и достаточно глубоко изучил HTML, CSS и JavaScript."
-                    />
-                    <AboutTextBlock
-                        title="Образование"
-                        text={text}
-                    />
+                    <div className="About__text-blocks">
+                        <AboutTextBlock
+                            title="Кратко обо мне"
+                            text="Программированием я начал интересоваться ещё в 8 классе и долгое время не мог определиться с направлением, поэтому на хорошем базовом уровне я владею несколькими языками программирования, в том числе: C#, Python и JS. В итоге я выбрал Front-end разработку в качестве направления своего развития и достаточно глубоко изучил HTML, CSS и JavaScript."
+                        />
+                        <AboutTextBlock
+                            title="Образование"
+                            text={text}
+                        />
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </section>
     );
 };
 
