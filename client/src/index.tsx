56ff676a8af9a542f6dc7fc79304972018bac66e
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-07-22 16:33:52 +0500

    i18next init

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import enResource from "./assests/languages/en.json";
+import ruResource from "./assests/languages/ru.json";
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {translation: enResource},
+            ru: {translation: ruResource}
+        },
+        lng: 'ru', // TODO: set to en
+        fallbackLng: 'ru', // TODO: set to en
+        interpolation: {escapeValue: false},
+        debug: true, // TODO: set to false
+    });
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
-
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 reportWebVitals();
