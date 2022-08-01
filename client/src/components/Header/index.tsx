c98bd0915650fa7c8ddd55034905313ae2e8103d
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-08-01 15:53:56 +0500

    first version of fixed header; variables.scss updated; variables.ts created but is not in use yet

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,12 +1,32 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import "../../styles/Header/Header.scss";
 import Container from "../Container";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
+import useTypedSelector from "../../hooks/useTypedSelector";
 
 const Header: FC = () => {
+    const [fixed, setFixed] = useState<boolean>();
+    const aboutOffset = useTypedSelector(state => state.offsets.about);
+
+    useEffect(() => {
+        function onScrollOrResize() {
+            setFixed(window.scrollY >= aboutOffset - 1);
+            console.log(window.scrollY);
+        }
+
+        window.addEventListener('scroll', onScrollOrResize);
+        window.addEventListener('resize', onScrollOrResize);
+        onScrollOrResize();
+
+        return function () {
+            window.removeEventListener('scroll', onScrollOrResize);
+            window.removeEventListener('resize', onScrollOrResize);
+        }
+    }, [aboutOffset])
+
     return (
-        <header className="Header">
+        <header className={"Header " + (fixed ? "_fixed" : "")}>
             <Container>
                 <div className="Header__inner">
                     <Logo/>
---
+++ client/src/styles/variables.ts
@@ -0,0 +1,39 @@
+// ATTENTION!
+// If you want to change this file, do not forget to make the same changes in variables.scss file
+
+export const FONTS = {
+    main: "Montserrat, sans-serif"
+}
+
+export const FONT_WEIGHTS = {
+    regular: 400,
+    medium: 500,
+    semi: 600,
+    bold: 700,
+    extra: 800,
+}
+
+export const SIZES = {
+    header: "6rem"
+}
+
+export const COLORS = {
+    black: "#333",
+    red: "#FE654F",
+    blue: "#4AD8F8",
+    yellow: "#FAF33E",
+    darkWhite: "#F7F7F7",
+}
+
+export const PATTERNS = {
+    gradientRed: "linear-gradient(242.56deg, #FF715E 12.86%, #FF4646 87.14%)",
+    gradientBlack: "linear-gradient(242.56deg, #3D3D3D 12.86%, #333333 87.14%)",
+    shadowPicture: "drop-shadow(-10px 10px 40px rgba(0, 0, 0, 0.2))",
+    shadowBlock: "0 5px 10px rgba(0, 0, 0, 0.15)",
+}
+
+export const TIMING = {
+    fast: 100,
+    standard: 300,
+    slow: 1200
+}
