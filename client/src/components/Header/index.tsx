e5d952f89bb859fca48540d868269890ef9cf547
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-08-01 20:21:14 +0500

    fixed header v3: fixed bug when updating the page

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import "../../styles/Header/Header.scss";
 import Container from "../Container";
 import Logo from "./Logo";
@@ -8,13 +8,41 @@ import {TIMING} from "../../styles/variables";
 
 const Header: FC = () => {
     const aboutOffset = useTypedSelector(state => state.offsets.about);
-    const [fixed, setFixed] = useState<boolean>(true);
+    const fixed = useRef<boolean>(false);
     const [classMod, setClassMod] = useState<string>("");
 
+    const ditMountCheck = useRef<boolean>(true);
     useEffect(() => {
         function onScrollOrResize() {
-            setFixed(window.scrollY >= aboutOffset - 1);
+            const newFixed = window.scrollY >= aboutOffset - 1;
+
+            // ditMount case, classes are set without pre- and post- statuses
+            if (ditMountCheck.current) {
+                ditMountCheck.current = false;
+
+                fixed.current = newFixed;
+                if (fixed.current) {
+                    setClassMod("_fixed")
+                }
+                return;
+            }
+
+            // general case
+            if (newFixed !== fixed.current){
+                fixed.current = newFixed;
+
+                if (fixed.current) {
+                    setClassMod("_preFixed")
+                    setTimeout(() => setClassMod("_fixed"), 20);
+                    return
+                }
+                setClassMod("_postFixed")
+                setTimeout(() => setClassMod(""), TIMING.standard);
+            }
         }
+
+        // Function is not working correctly before offsets was stated in Redux
+        if (!aboutOffset) return;
 
         window.addEventListener('scroll', onScrollOrResize);
         window.addEventListener('resize', onScrollOrResize);
@@ -25,16 +53,6 @@ const Header: FC = () => {
             window.removeEventListener('resize', onScrollOrResize);
         }
     }, [aboutOffset])
-
-    useEffect(() => {
-        if (fixed) {
-            setClassMod("_preFixed")
-            setTimeout(() => setClassMod("_fixed"), 20);
-            return
-        }
-        setClassMod("_postFixed")
-        setTimeout(() => setClassMod(""), TIMING.standard);
-    }, [fixed])
 
     return (
         <header className={"Header " + classMod}>
