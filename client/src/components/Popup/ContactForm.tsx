2aaecb6af7e1cedbe0b003cbae8d0e87ac55f788
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-08-04 17:56:41 +0500

    emailJS v1

--- client/src/components/Popup/ContactForm.tsx
+++ client/src/components/Popup/ContactForm.tsx
@@ -1,9 +1,10 @@
-import React, {FC, useRef, useState} from 'react';
+import React, {FC, useRef} from 'react';
 import "../../styles/Popup/ContactForm.scss";
 import {useDispatch} from "react-redux";
 import {TPopupReducerActionType} from "../../types/popup";
 import {useTranslation} from "react-i18next";
 import useCustomScrollbar from "../../hooks/useCustomScrollbar";
+import emailJS from "emailjs-com";
 
 const ContactForm: FC = () => {
     const {t} = useTranslation();
@@ -11,47 +12,60 @@ const ContactForm: FC = () => {
     const customScrollElement = useRef<HTMLTextAreaElement>(null);
     useCustomScrollbar(customScrollElement);
 
-    const [name, setName] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
-    const [message, setMessage] = useState<string>("");
-
     const dispatch = useDispatch();
-    function send() {
-        // TODO: send data on api
+    const form = useRef<HTMLFormElement>(null);
+    function sendMessage() {
+        if (!form.current) return;
+
+        console.log("submit");
+
+        emailJS.sendForm(
+            "service_4kogcbn",
+            "template_jkfg53r",
+            form.current,
+            "16FU0KEkZHu91z_nC"
+        )
+            .then(response => {
+                // TODO: notify user
+                console.log(response);
+            })
+            .catch(error => {
+                // TODO: notify user
+                console.log(error);
+            })
+
         dispatch({type: TPopupReducerActionType.SET_ACTIVE, payload: false})
     }
 
     return (
-        <div className="ContactForm">
+        <form className="ContactForm" ref={form}>
             <input
+                name="name"
                 className="ContactForm__input"
                 type="text"
                 placeholder={t("contactForm.name")}
-                value={name}
-                onChange={event => setName(event.target.value)}
             />
             <input
+                name="contact"
                 className="ContactForm__input"
                 type="text"
                 placeholder={t("contactForm.email")}
-                value={email}
-                onChange={event => setEmail(event.target.value)}
             />
             <textarea
+                name="message"
                 className="ContactForm__textarea"
                 ref={customScrollElement}
                 rows={8}
                 placeholder={t("contactForm.message")}
-                value={message}
-                onChange={event => setMessage(event.target.value)}
             />
             <button
+                type="submit"
                 className="ContactForm__button"
-                onClick={send}
+                onClick={sendMessage}
             >
                 {t("contactForm.button")}
             </button>
-        </div>
+        </form>
     );
 };
 
