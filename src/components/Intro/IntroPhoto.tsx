20909a3406c50c085bba8878d3ccc492c3e75ce0
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-08-13 20:44:19 +0500

    intro adaptation: IntroPhoto max-height

--- src/components/Intro/IntroPhoto.tsx
+++ src/components/Intro/IntroPhoto.tsx
@@ -1,24 +1,31 @@
 import React, {FC} from 'react';
+import "../../styles/Intro/IntroPhoto.scss";
 import MySvg from '../../assests/MySvg';
 import {TMySvgId} from "../../types";
-import "../../styles/Intro/IntroPhoto.scss";
 import ImageInsideVector from "../ImageInsideVector";
 import photo from "../../assests/pictures/my-photo-1.jpg";
 
 const IntroPhoto: FC = () => {
+    const svgId = TMySvgId.maskFrame1;
+    const viewBox = "0 0 100 94";
+
     return (
         <div className="IntroPhoto">
+            <svg
+                className="IntroPhoto__size-box"
+                viewBox={viewBox}
+            />
             <ImageInsideVector
                 className="IntroPhoto__picture"
                 imageURL={photo}
                 maskVector={
-                    <MySvg Id={TMySvgId.maskFrame1} fill={{color: "#fff", opacity: 1}}/>
+                    <MySvg Id={svgId} fill={{color: "#fff", opacity: 1}}/>
                 }
-                viewBox="0 0 100 94"
+                viewBox={viewBox}
             />
             <MySvg
                 className="IntroPhoto__border"
-                Id={TMySvgId.maskFrame1}
+                Id={svgId}
                 stroke={{
                     color: "#fff",
                     opacity: 0.8,
