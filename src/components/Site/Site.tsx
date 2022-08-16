b1233822f88050863f614dd5b4fcb0b72049fa34
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-08-16 16:37:31 +0500

    site section background on apple devices fix

--- src/components/Site/Site.tsx
+++ src/components/Site/Site.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useRef} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import "../../styles/Site/Site.scss";
 import Container from "../Container";
 import SectionTitle from "../SectionTitle";
@@ -6,6 +6,7 @@ import {useTranslation} from "react-i18next";
 import {TOffsetsReducerActionType} from "../../types/offsets";
 import useSetOffset from "../../hooks/useSetOffset";
 import SiteAnimationBlock from "./SiteAnimationBlock";
+import {COLORS} from "../../styles/variables";
 
 const Site: FC = () => {
     const {t} = useTranslation();
@@ -13,8 +14,17 @@ const Site: FC = () => {
     const offsetCheckoutBlock = useRef<HTMLDivElement>(null);
     useSetOffset(offsetCheckoutBlock, TOffsetsReducerActionType.SET_WORKS);
 
+    const [apple, setApple] = useState<boolean>(false);
+    useEffect(() => {
+        setApple(navigator.userAgent.toLowerCase().search(/(ipad|iphone|mac os|macintosh)/) !== -1)
+    }, [])
+
     return (
-        <section className="Site" ref={offsetCheckoutBlock}>
+        <section
+            className="Site"
+            ref={offsetCheckoutBlock}
+            style={apple ? {background: COLORS.lightBlack} : {}}
+        >
             <Container>
                 <div className="Site__inner">
                     <SectionTitle
